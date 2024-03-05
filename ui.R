3edbc7044ca8c3984458b578cb3b5bab432d7fc2
lilyttngo <160081508+lilyttngo@users.noreply.github.com>
2024-03-05 14:54:20 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,12 +1,12 @@
 library("shiny")
 library("shinythemes")
 library("plotly")
-source()
-ui<-fluidPage(
-  theme=shinytheme("flatly"),
-  titlePanel("Employment Rates by Educational Attainment")，
+
+ui <- fluidPage(
+  theme = shinytheme("flatly"),
+  titlePanel("Employment Rates by Educational Attainment"),
   tabsetPanel(
-    tabPanel("Project Overview",fluid=TRUE,sidebarLayout(
+    tabPanel("Project Overview", fluid = TRUE, sidebarLayout(
       sidebarPanel(
         h1("BACKGROUND | The Unseen Dynamics of Education and Employment In America"),
         p("In a rapidly evolving society, where the connection between education 
@@ -15,22 +15,26 @@ ui<-fluidPage(
           and its profound impact on career trajectories."),
         p("Our project addresses fundamental life issues like education and employment
           which impact millions of Americans. The struggles and successes of individuals 
-          navigating the education-to-emplopyment pipeline resonate with a broad audience,
-          making the project universally relatable.")
+          navigating the education-to-employment pipeline resonate with a broad audience,
+          making the project universally relatable."),
         h2("RESEARCH QUESTION | Questions that We Should Consider"),
-        p("The",strong("overarching research question"),"for this project is:
-          How Much Are People Making Per Year Based On How Educated They Are?")，
-        p("-What is the average annual income for individuals with different levels of education, 
+        p("The", strong("overarching research question"), "for this project is:
+          How Much Are People Making Per Year Based On How Educated They Are?"),
+        p("- What is the average annual income for individuals with different levels of education, 
           such as high school diplomas, bachelor's, master's, and doctoral degree?"),
-        p("-How have changes in education levels over time impacted median household income across 
+        p("- How have changes in education levels over time impacted median household income across 
           different rural and urban areas?"),
-        p(-"What is the correlation between the proportion of adults with a bachelor's degree or
+        p("- What is the correlation between the proportion of adults with a bachelor's degree or
           higher and median household income in different countries, and how does this vary
-          across states?")，
-        p(-"How have changes in education funding and policy impacted employment 
-          rates and income levels over the last few decades?")，
-        p(-"How does the distribution of education levels vary across different regions, and 
-        what impact does this have on regional economic development?"),
+          across states?"),
+        p("- How have changes in education funding and policy impacted employment 
+          rates and income levels over the last few decades?"),
+        p("- How does the distribution of education levels vary across different regions, and 
+          what impact does this have on regional economic development?")
+      ),
+      mainPanel(
+        
+      )
+    ))
   )
-    )))
-  )
+)
