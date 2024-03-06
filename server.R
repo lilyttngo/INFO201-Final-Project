a52a38bdb309f4b7ae3427ce532777a2cbdd84ee
ConnorYan <92060074+ConnorYan@users.noreply.github.com>
2024-03-05 23:33:52 -0800

    server.R

--- server.R
+++ server.R
@@ -2,42 +2,12 @@ library(ggplot2)
 library(plotly)
 library(dplyr)
 
-# Read in data
-df <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/NationalNames.csv?raw=true")
+# Source file with dataframe
 
-# Filter the data in some way (aka pick a subset of names to examine — it's too large to include all names for all years)
-top_names <- df %>% 
-  group_by(Name) %>% 
-  summarize(total = sum(Count)) %>% 
-  slice_max(n = 100, order_by = total)
 
-subset_df <- df %>% filter(Name %in% top_names$Name)
 
+# Server
 server <- function(input, output) {
-
-  output$names_plot <- renderPlotly({
-
-    filtered_df <- subset_df %>% 
-      # Filter for user's gender selection
-      filter(Gender %in% input$gender_selection) %>%
-      # Filter for user's name selection
-      filter(Name %in% input$name_selection) %>% 
-      # Filter for user's year selection
-      filter(Year > input$year_selection[1] & Year < input$year_selection[2])
-    
-    # Line plot
-    names_plot <- ggplot(data = filtered_df) +
-      geom_line(mapping = 
-                  aes(x = Year, 
-                      y = Count, 
-                      color = Name))
-
-    return(names_plot)
-    
-  })
-
-}
-
 #Viz 3
 # Render plot for Question 1
   output$viz_3_output_education <- renderPlotly({
@@ -52,7 +22,7 @@ ggplot(results_df, aes(x = Year, y = Impact_on_Unemployment, color = Education_L
   labs(y = "Impact on Unemployment Rates", title = "Impact of Education Levels on Unemployment Rates Over Time") +
   theme_minimal()
   })
-
+}
   # Render plot for Question 2
   output$viz_3_output_income <- renderPlotly({
     # ggplot(education_impact_df, aes(x = Year, y = Impact_on_Median_Income, color = Area_Type)) +
